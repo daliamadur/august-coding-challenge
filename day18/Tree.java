b863ae1fb5f3378d19f7b679bf8a01cc037889bd
Chel <91010394+lishanlung@users.noreply.github.com>
2024-10-26 16:06:49 +0100

    Create Tree.java

---
+++ day18/Tree.java
@@ -0,0 +1,10 @@
+class Tree<T> {
+    private TreeNode<T> root;
+
+    Tree(TreeNode<T> root) { this.root = root; }
+
+    public TreeNode<T> getRoot() { return this.root; }
+    
+    public void depthFirstSearch(TreeNode<T> target) {}
+
+    public void breadthFirstSearch(TreeNode<T> target) {}
